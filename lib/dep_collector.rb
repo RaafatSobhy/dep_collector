86c746458d79ab0428cecf7dbf0e719f8d977325
RaafatSobby <raafat@2loops.com>
2018-01-14 00:10:58 +0200

    add class and method in it to collect the dep -- ADDING AN EXECUTABLE file bin/console

--- lib/dep_collector.rb
+++ lib/dep_collector.rb
@@ -1,5 +1,26 @@
 require "dep_collector/version"
 
 module DepCollector
-  # Your code goes here...
+  class DependencyCommand
+    require 'rubygems/commands/dependency_command'
+
+    def self.get_dependencies name, local = true
+      cmd = Gem::Commands::DependencyCommand.new
+
+      dependency = cmd.gem_dependency name, nil, nil
+
+      specs =
+          if local
+            dependency.matching_specs.uniq.sort
+          else
+            cmd.fetch_remote_specs(dependency).uniq.sort
+          end
+
+      dependencies = []
+      specs.each do |spec|
+        dependencies.concat spec.dependencies.sort_by {|dep| dep.name}.map {|dep| [dep.name, dep.requirement]}
+      end
+      dependencies
+    end
+  end
 end
